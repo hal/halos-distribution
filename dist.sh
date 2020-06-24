3e4604706697dd3912bc6701fe4fd6e42dbc7a28
Harald Pehl <harald.pehl@gmail.com>
2020-06-24 16:06:36 +0200

    Add newlines to dist script

--- dist.sh
+++ dist.sh
@@ -19,19 +19,19 @@ HALOS_CONSOLE=$BASEDIR/halos-console
 HALOS_PROXY=$BASEDIR/halos-proxy
 
 
-echo "Assemble halOS console"
-cd "$HALOS_CONSOLE" || { echo "halOS console directory at '$HALOS_CONSOLE' not found" ; exit 1; }
+printf "Assemble halOS console\n"
+cd "$HALOS_CONSOLE" || { printf "halOS console directory at '$HALOS_CONSOLE' not found" ; exit 1; }
 ./gradlew assemble
 
 
-echo "Install halOS console"
+printf "\n\n\nInstall halOS console\n"
 mvn install
 
 
-echo "Install halOS proxy"
-cd "$HALOS_PROXY" || { echo "halOS proxy directory at '$HALOS_PROXY' not found" ; exit 1; }
-mvn install -Dquarkus.container-image.registry=quay.io -Dquarkus.container-image.group=halconsole -Dquarkus.container-image.name=halos -Dquarkus.container-image.tag=latest -Dquarkus.container-image.insecure=true
+printf "\n\n\nInstall halOS proxy\n"
+cd "$HALOS_PROXY" || { printf "halOS proxy directory at '$HALOS_PROXY' not found" ; exit 1; }
+mvn install -Dquarkus.container-image.build=true -Dquarkus.container-image.push=true -Dquarkus.container-image.registry=quay.io -Dquarkus.container-image.group=halconsole -Dquarkus.container-image.name=halos -Dquarkus.container-image.tag=latest -Dquarkus.container-image.insecure=true
 
 
 cd "$ROOT" || exit 1
-echo "DONE"
+printf "\n\n\nDONE\n"
